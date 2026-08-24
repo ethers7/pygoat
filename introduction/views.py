1b6fb53e1599ac80e05c28c99cba71c316f36923
Harness Auto-Remediation <remediation@harness.io>
2026-08-24 07:52:48 +0000

    vuln-fix: Replace native XML libs with defusedxml to prevent (STO-rv_L4LSoWjfde3lWY9u45B)
    Replace xml.dom.pulldom and xml.sax imports with defusedxml equivalents to prevent XML External Entity (XXE) attacks. defusedxml disables external entities by default.
    
    Weakness: 611
    Severity: High
    Detection: Tool
    Signed-off-by: Harness Auto-Remediation <remediation@harness.io>
    Co-authored-by: Harness Auto-Remediation <remediation@harness.io>

--- introduction/views.py
+++ introduction/views.py
@@ -13,9 +13,9 @@ from dataclasses import asdict, dataclass
 from hashlib import md5
 from io import BytesIO
 from random import randint
-from xml.dom.pulldom import START_ELEMENT, parseString
-from xml.sax import make_parser
-from xml.sax.handler import feature_external_ges
+from xml.dom.pulldom import START_ELEMENT
+
+from defusedxml.pulldom import parseString
 
 import jwt
 import requests
@@ -254,9 +254,7 @@ def xxe_see(request):
 @csrf_exempt
 def xxe_parse(request):
 
-    parser = make_parser()
-    parser.setFeature(feature_external_ges, True)
-    doc = parseString(request.body.decode('utf-8'), parser=parser)
+    doc = parseString(request.body.decode('utf-8'))
     for event, node in doc:
         if event == START_ELEMENT and node.tagName == 'text':
             doc.expandNode(node)
