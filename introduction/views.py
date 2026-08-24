ed146fd9ffd70d43ad13d7a851f42f928a598129
Harness Auto-Remediation <remediation@harness.io>
2026-08-24 08:29:17 +0000

    vuln-fix: Replace xml.sax with defusedxml.sax for XXE protec (STO-rv_L4LSoWjfde3lWY9u45B)
    Replace native xml.sax.make_parser with defusedxml.sax.make_parser which disables external entities by default. Remove explicit feature_external_ges import and the dangerous setFeature call that enabled XXE.
    
    Weakness: 611
    Severity: High
    Detection: Tool
    Signed-off-by: Harness Auto-Remediation <remediation@harness.io>
    Co-authored-by: Harness Auto-Remediation <remediation@harness.io>

--- introduction/views.py
+++ introduction/views.py
@@ -15,8 +15,8 @@ from hashlib import md5
 from io import BytesIO
 from random import randint
 from xml.dom.pulldom import START_ELEMENT, parseString
-from xml.sax import make_parser
-from xml.sax.handler import feature_external_ges
+
+from defusedxml.sax import make_parser
 
 import jwt
 import requests
@@ -256,7 +256,6 @@ def xxe_see(request):
 def xxe_parse(request):
 
     parser = make_parser()
-    parser.setFeature(feature_external_ges, True)
     doc = parseString(request.body.decode('utf-8'), parser=parser)
     for event, node in doc:
         if event == START_ELEMENT and node.tagName == 'text':
