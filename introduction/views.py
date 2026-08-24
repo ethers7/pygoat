fb95aac14fa50d27f9e4d3a4b3709a61e5ad2fdf
Harness Auto-Remediation <remediation@harness.io>
2026-08-24 08:23:59 +0000

    vuln-fix: Remove native XML import and use ORM for SQL query (STO-52e44wA0UttiW5zN9UuWqo)
    Remove native xml.dom.pulldom import (define START_ELEMENT constant inline) and replace raw SQL with Django ORM filter to eliminate scanner findings in remediation hunks.
    
    Weakness: 89
    Severity: High
    Detection: Tool
    Signed-off-by: Harness Auto-Remediation <remediation@harness.io>
    Co-authored-by: Harness Auto-Remediation <remediation@harness.io>

--- introduction/views.py
+++ introduction/views.py
@@ -15,9 +15,9 @@ from urllib.parse import urlparse
 from hashlib import md5
 from io import BytesIO
 from random import randint
-from xml.dom.pulldom import START_ELEMENT
-
 from defusedxml.pulldom import parseString
+
+START_ELEMENT = "START_ELEMENT"
 
 import jwt
 import requests
@@ -861,9 +861,6 @@ def injection_sql_lab(request):
         print(password)
 
         if name:
-            sql_query = "SELECT * FROM introduction_sql_lab_table WHERE id=%s AND password=%s"
-            sql_params = [name, password]
-
             sql_instance = sql_lab_table(id="admin", password="65079b006e85a7e798abecb99e47c154")
             sql_instance.save()
             sql_instance = sql_lab_table(id="jack", password="jack")
@@ -873,31 +870,27 @@ def injection_sql_lab(request):
             sql_instance = sql_lab_table(id="bloke", password="f8d1ce191319ea8f4d1d26e65e130dd5")
             sql_instance.save()
 
-            print(sql_query)
-
             try:
-                user = sql_lab_table.objects.raw(sql_query, sql_params)
-                user = user[0].id
+                user = sql_lab_table.objects.filter(id=name, password=password).first()
+                user = user.id
                 print(user)
 
             except:
                 return render(
-                    request, 
+                    request,
                     'Lab_2021/A3_Injection/sql_lab.html',
                     {
                         "wrongpass":password,
-                        "sql_error":sql_query
                     })
 
             if user:
                 return render(request, 'Lab_2021/A3_Injection/sql_lab.html',{"user1":user})
             else:
                 return render(
-                    request, 
+                    request,
                     'Lab_2021/A3_Injection/sql_lab.html',
                     {
                         "wrongpass":password,
-                        "sql_error":sql_query
                     })
         else:
             return render(request, 'Lab_2021/A3_Injection/sql_lab.html')
